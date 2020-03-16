890ce0f2b6263078b4a942ac8ed5dffe5eb162d8
Mathias Lantean <mathias.lantean@rootstrap.com>
2020-03-16 15:04:04 -0300

    Remove missing $ at the end of URL

--- profile/urls.py
+++ profile/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('', include('allauth.account.urls')),
     path('registration/', include(('dj_rest_auth.registration.urls', 'auth'))),
-    path('facebook/$', FacebookLogin.as_view(), name='fb_login'),
+    path('facebook/', FacebookLogin.as_view(), name='fb_login'),
 ]
