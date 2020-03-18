819895f55941d54e92d39de4f5bbae6253d630b1
Mathias Lantean <mathias.lantean@rootstrap.com>
2020-03-18 12:28:37 -0300

    Fix typo in test case

--- profile/tests/test_views.py
+++ profile/tests/test_views.py
@@ -156,7 +156,7 @@ class UserAuthTests(TestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_reset_password_wron_password_format(self):
+    def test_reset_password_wrong_password_format(self):
         url = reverse('rest_password_reset_confirm')
         data = {
             "new_password1": "test",
