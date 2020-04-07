4cf09cc7b6fd6dbf9c9f0a627e95905a67b03490
Mathias Lantean <mathias.lantean@rootstrap.com>
2020-04-07 13:16:48 -0300

    Add limit for maximum of targets per user
    Common users just can add as many targets as it is defined in the project settings.

--- target/serializers.py
+++ target/serializers.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from .models import Target, Topic
@@ -10,9 +11,19 @@ class TargetSerializer(GeoFeatureModelSerializer):
         read_only_fields = ['user']
         geo_field = 'location'
 
+    def check_max_num_of_targets(self, user):
+        num_current_targets = Target.objects.filter(user=user).count()
+        if num_current_targets >= settings.MAX_NUMBER_OF_TARGETS:
+            msg = f"It is not possible to create more than {settings.MAX_NUMBER_OF_TARGETS} targets."
+            raise serializers.ValidationError(msg)
+
     def create(self, validated_data):
         request = self.context.get('request')
         validated_data['user'] = request.user
+
+        if not request.user.is_superuser:
+            self.check_max_num_of_targets(request.user)
+
         return super(TargetSerializer, self).create(validated_data)
 
 
