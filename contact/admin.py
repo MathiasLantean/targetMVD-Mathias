706cb28e79fd90b92e95f1c356fa16e3f3a3fcd7
Mathias Lantean <mathias.lantean@rootstrap.com>
2020-04-09 16:14:53 -0300

    Add Information model
    This model will save long texts that are likely to change, so admins can change them from the Django admin.
    Each Info section will have only one instance and it must be created through data migrations, Django admin only allows admins to edit that instance (neither create nor delete).

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from contact.models import Information
 
-# Register your models here.
+
+class InformationAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(Information, InformationAdmin)
