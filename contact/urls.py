a597be32a69e91ccbe07244629d870f24ad7b13e
Mathias Lantean <mathias.lantean@rootstrap.com>
2020-04-12 01:07:53 -0300

    Add API for retrieving general information
    This API is used for getting text and system information that should be stored in the DB in order to make them easier to change by admins.

--- contact/urls.py
+++ contact/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+
+from contact.views import InformationDetail
 
 urlpatterns = [
+    path('information/<slug:pk>/', InformationDetail.as_view(), name='info'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from contact.models import Information
+from contact.serializers import InformationSerializer
+
+
+class InformationDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Information.objects.get(pk=pk)
+        except Information.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        info = self.get_object(pk)
+        serializer = InformationSerializer(info)
+        return Response(serializer.data)
