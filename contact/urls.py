728e439bb55d1b2b2dbd17002c5bf841501a16be
Mathias Lantean <mathias.lantean@rootstrap.com>
2020-04-13 13:40:59 -0300

    Add view for asking questions to admin users
    This API is not related to any model. The question is just sent by email to all the admin users in the system.

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,12 @@
+from django.urls import path, include
 from rest_framework import routers
 
-from contact.views import InformationDetail
+from contact.views import InformationDetail, SendQuestion
 
 router = routers.SimpleRouter()
 router.register(r'information', InformationDetail, basename='info')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('questions/', SendQuestion.as_view(), name='send_question'),
+    path('', include(router.urls)),
+]
